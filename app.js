1df8da41f77dc242dcca99d4779172ef19fb47c3
Daniel Totten <TottenDC@gmail.com>
2018-12-14 10:46:09 -0500

    Sets up app with basic functionality.
    -Adds img folder to public folder
    -Adds '/about' to href in layout.pug

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const data = require('./data.json');
+
+const app = express();
+
+app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index', data.projects);
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/project/:id', (req, res) => {
+  const { id } = req.params;
+  const templateData = data.projects[id];
+
+  res.render('project', templateData);
+});
+
+app.listen(3000, () => console.log('The app is running on port 3000.'));
