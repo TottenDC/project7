5387acb2833b78a978d5323efd0046211b4d6353
Daniel Totten <TottenDC@gmail.com>
2018-12-14 14:17:40 -0500

    Fixes locals input from data.json
    -Correctly formats data as objects
    -Updates index.pug to dynamically add projects
    -Changes image urls to come from static route
    -Adds contact info to about.pug

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index', data.projects);
+  const { projects } = data;
+  const templateData = { projects }
+  res.render('index', templateData);
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
-app.get('/project/:id', (req, res) => {
+app.get('/project:id', (req, res) => {
   const { id } = req.params;
-  const templateData = data.projects[id];
+  const project = data.projects[id];
+  const templateData = { project };
 
   res.render('project', templateData);
 });
