d375887cb513c4a4b17046c327d892249d04d328
Daniel Totten <TottenDC@gmail.com>
2018-12-19 16:09:24 -0500

    Add comments.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+/* --- Variables and Modules --- */
 const express = require('express');
 const data = require('./data.json');
-
 const app = express();
 
+/* --- Setup --- */
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
+/* --- Routing --- */
 app.get('/', (req, res) => {
   const { projects } = data;
   const templateData = { projects }
@@ -14,6 +16,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about');
 });
+// Project route. Redirects to error page if user goes to non-existant project ID.
 app.get('/project:id', (req, res) => {
   const { id } = req.params;
   if (id >= data.projects.length) {
@@ -21,16 +24,17 @@ app.get('/project:id', (req, res) => {
   }
   const project = data.projects[id];
   const templateData = { project };
-
   res.render('project', templateData);
 });
 
+/* --- Error Handling --- */
+// Routing catcher for anything not specified above. Passes to error middleware.
 app.use((req, res, next) => {
   const err = new Error('Page Not Found');
   err.status = 404;
   next(err);
 });
-
+// Error middleware. It will redirect to special error page.
 app.use((err, req, res, next)=>{
   console.log(`Sorry, there was an error (${err.message}). Please try again.`);
   res.locals.error = err;
@@ -38,4 +42,5 @@ app.use((err, req, res, next)=>{
   res.render('error');
 });
 
+/* --- Server startup --- */
 app.listen(3000, () => console.log('The app is running on port 3000.'));
